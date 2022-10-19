9eaaa0ae556128d552cc3bc249b3751ea3b1573f
Maksim_Troshkov <smart22108@mail.ru>
2022-10-19 23:12:05 +0300

    feat(package.json): Установил firebase на проект и настроил подключение его и плагина сообщений

--- src/main.js
+++ src/main.js
@@ -2,17 +2,41 @@ import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
+import messagePlugin from '@/common/message.plugin'
 import dateFilter from '@/filters/date.filter'
 import router from './router'
 import store from './store'
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyA2YJvyoU0y7ZPQHS6YhHbobCtlgTDlLgA",
+  authDomain: "vue-crm-7cf75.firebaseapp.com",
+  projectId: "vue-crm-7cf75",
+  storageBucket: "vue-crm-7cf75.appspot.com",
+  messagingSenderId: "1053849106179",
+  appId: "1:1053849106179:web:30ec55a55332a29ebf1b36",
+  measurementId: "G-3PYPK8LBR9"
+});
+
+let app 
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
